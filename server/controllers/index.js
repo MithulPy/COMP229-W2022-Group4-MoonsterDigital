887e6cec4bfdbefe205a2dbd10319eaa4ec7d3df
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-13 20:27:39 -0400

    Fix the index controller to display home page, login and register

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,8 +10,41 @@
 let express = require('express');
 let router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Welcome to Tournament Page');
-  });
-  
-module.exports = router;
+module.exports.displayHomePage = (req, res, next) => {
+  res.render('index', { title: 'Home', displayName: /*req.user ? req.user.displayName :*/ ''});
+};
+
+module.exports.displayLoginPage = (req, res, next) => {
+  // check if user is already logged in
+  if (!req.user)
+  {
+      res.render('auth/login', 
+      {
+          title: 'Login',
+          messages: /*req.flash('loginMessage')*/ '',
+          displayName: /*req.user ? req.user.displayName :*/ ''
+      });
+  }
+  else
+  {
+      res.redirect('/');
+  }
+};
+
+module.exports.displayRegisterPage = (req, res, next) => {
+  // check if user already logged in
+  if (!req.user)
+  {
+      
+      res.render('auth/register',
+      {
+          title: 'Register',
+          messages: /*req.flash('registerMessages')*/ '',
+          displayName: /*req.user ? req.user.displayName :*/ ''
+      });
+  }
+  else
+  {
+      return res.redirect('/');
+  }
+};
