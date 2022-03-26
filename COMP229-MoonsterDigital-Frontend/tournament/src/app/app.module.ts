4558f9bccbeecac4933096adfaa5e50fe0aaef0f
MithulPy <mithulofficial@gmail.com>
2022-03-26 22:34:35 +0300

    Changes to routes pages

--- COMP229-MoonsterDigital-Frontend/tournament/src/app/app.module.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home/home.component';
-import { AddTournamentComponent } from './pages/add-tournament/add-tournament/add-tournament.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AddTournamentComponent } from './pages/add-tournament/add-tournament.component';
+import { MainNavComponent } from './partials/main-nav/main-nav.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { TournamentListComponent } from './tournament-list/tournament-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AddTournamentComponent
+    AddTournamentComponent,
+    MainNavComponent,
+    FooterComponent,
+    HeaderComponent,
+    TournamentListComponent
   ],
   imports: [
     BrowserModule,
