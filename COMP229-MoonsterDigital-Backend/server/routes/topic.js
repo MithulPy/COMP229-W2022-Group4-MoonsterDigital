733f0c8d0a200e169fbf5853c9b966a0e7dc4fff
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-29 16:48:53 -0400

    Fix topicRouter and CommentRouter in backend

--- COMP229-MoonsterDigital-Backend/server/routes/topic.js
+++ COMP229-MoonsterDigital-Backend/server/routes/topic.js
@@ -10,16 +10,16 @@
 var express = require('express');
 var router = express.Router();
   
-let forumController = require('../controllers/topic');
+let topicController = require('../controllers/topic');
 
  
 /** Show Forum Topics available  */
-router.get('/list', forumController.displayTopics);
+router.get('/list', topicController.displayTopics);
 
-/* Post Route for the Edit tournament page - Update Operation */
-router.post('/edit/:id', forumController.processEditPage);
+/* Post Route for the Edit topic page - Update Operation */
+router.post('/edit/:id', topicController.processEditPage);
 
-/* Post Route for the ADD tournament page - Update Operation */
-router.post('/add', forumController.processAddPage);
+/* Post Route for the ADD topic page - Update Operation */
+router.post('/add', topicController.processAddPage);
  
 module.exports = router;
