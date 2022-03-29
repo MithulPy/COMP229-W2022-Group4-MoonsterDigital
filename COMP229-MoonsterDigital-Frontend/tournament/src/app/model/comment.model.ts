4b9fc05e36ea40107101858c810f19861425d6a0
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-29 15:12:30 -0400

    Update topic and comment models in frontend

---
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/model/comment.model.ts
@@ -0,0 +1,31 @@
+/**
+ * @GroupName MoonsterDigital
+ * @FileName COMP229_W2022-Group4-MoonsterDigital
+ * @CourseCode COMP229
+ * @Date Mar 29th 2022
+ * @CourseName Web Application Development SEC005
+ */
+
+ export class Comment{
+    constructor(
+        public _id?:number,
+        public title?:String,
+        public date?:Date,
+        public content?:String,
+        public username?:String,
+    ){}
+
+    public toString(): string
+    {
+        return `
+        Comment
+        -------------------------------
+        Id         : ${this._id}
+        Title      : ${this.title}
+        Date       : ${this.date}
+        Content    : ${this.content}
+        Username   : ${this.username}
+        -------------------------------
+        `;
+    }
+}
