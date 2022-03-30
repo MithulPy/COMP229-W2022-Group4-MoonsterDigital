3a0a337723fc7a0000712b9c7ecd56e875659714
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-29 20:14:00 -0400

    Try to fix register ts

--- COMP229-MoonsterDigital-Frontend/tournament/src/app/admin/register/register.component.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/admin/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule }  from  '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TournamentRepo } from 'src/app/model/tournament.repository';
@@ -10,13 +11,20 @@ import { TournamentRepo } from 'src/app/model/tournament.repository';
 })
 export class RegisterComponent implements OnInit {
 
+  userForm!: FormGroup;
+
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
-    private repository: TournamentRepo) { }
-    userForm!: FormGroup;
+    private repository: TournamentRepo) {
+      this.createUserForm();
+    }
 
     ngOnInit() {
+      
+    }
+
+    createUserForm(){
       this.userForm = this.formBuilder.group({
         id: [],
         username: ['', Validators.required],
