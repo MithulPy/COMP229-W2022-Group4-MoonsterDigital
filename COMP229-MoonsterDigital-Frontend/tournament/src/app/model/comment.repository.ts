ac17f201f8ed0e3b0a829ec5b59c01d83d6d26e1
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-30 18:58:10 -0400

    Add comment.repository

---
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/model/comment.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Comment } from './comment.model';
+import { StaticDataSource } from './static.datasource';
+import { User } from './user.model';
+import { RestDataSource } from './rest.datasource';
+
+@Injectable()
+export class CommentRepo {
+    private comments: Comment[] = [];
+
+    constructor(private dataSource: RestDataSource) {
+        dataSource.getComments().subscribe(data => {
+            this.comments = data;
+        });
+    }
+
+    getComments() {
+        return this.comments;
+    }
+
+    getCommentsByTopicId(topicId: any){
+        return this.comments.filter(a => a.topicId === topicId);
+    }
+
+    getComment(id: any)
+    {
+        return this.comments.find(b => b._id == id)!;
+    }
+
+    saveComment(savedComment: Comment): void {
+        if (savedComment._id === null || savedComment._id === 0 || savedComment._id === undefined) {
+            this.dataSource.addComment(savedComment).subscribe(b => {
+                this.comments.push(savedComment);
+            });
+        }
+    }
+}
