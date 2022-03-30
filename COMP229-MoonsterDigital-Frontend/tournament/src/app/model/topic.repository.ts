3b74d9ecb40d43357371bceca39aaa20c3641395
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-30 01:44:20 -0400

    Add topic.repository

---
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/model/topic.repository.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Topic } from './topic.model';
+import { StaticDataSource } from './static.datasource';
+import { User } from './user.model';
+import { RestDataSource } from './rest.datasource';
+
+@Injectable()
+export class TopicRepo {
+    private topics: Topic[] = [];
+
+    constructor(private dataSource: RestDataSource) {
+        dataSource.getTopics().subscribe(data => {
+            this.topics = data;
+        });
+    }
+
+    getTopics() {
+        return this.topics;
+    }
+
+    getTopic(id: any)
+    {
+        return this.topics.find(b => b._id == id)!;
+    }
+
+    saveTopic(savedTopic: Topic): void {
+        if (savedTopic._id === null || savedTopic._id === 0 || savedTopic._id === undefined) {
+            this.dataSource.addTopic(savedTopic).subscribe(b => {
+                this.topics.push(savedTopic);
+            });
+        }
+    }
+}
