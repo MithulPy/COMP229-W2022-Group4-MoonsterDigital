17b7c62c18e75b4ac62ac9745cc1c7a7ad74784a
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-03-29 20:15:05 -0400

    Update login and register routing in app-routing

--- COMP229-MoonsterDigital-Frontend/tournament/src/app/app-routing.module.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './pages/home/home.component';
+import { AuthComponent } from './admin/auth/auth.component';
+import { RegisterComponent } from './admin/register/register.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
+  {path: 'login', component: AuthComponent ,data: {title: 'Login'}},
+  {path: 'register', component: RegisterComponent, data: {title: 'Register'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', redirectTo: '/home', pathMatch: 'full'}
 
