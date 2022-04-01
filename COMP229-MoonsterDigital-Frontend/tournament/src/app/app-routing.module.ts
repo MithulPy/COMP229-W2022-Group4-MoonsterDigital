365946ee628b88775a3734b6653e775821ca2fd0
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-04-01 18:34:33 -0400

    Update app.module and app-routing after deleting unused pages and updating login and register pages

--- COMP229-MoonsterDigital-Frontend/tournament/src/app/app-routing.module.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/app-routing.module.ts
@@ -3,13 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
 import { HomeComponent } from './pages/home/home.component';
-import { AuthComponent } from './admin/auth/auth.component';
-import { RegisterComponent } from './admin/register/register.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, data: { title: 'Home' } },
-  { path: 'login', component: AuthComponent, data: { title: 'Login' } },
-  { path: 'register', component: RegisterComponent, data: { title: 'Register' } },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
   { path: 'forum', loadChildren: () => import('./forum/forum.module').then(m => m.ForumModule)},
   { path: 'tournament', loadChildren: () => import('./tournament/tournament.module').then(m => m.TournamentModule)},
   { path: '', redirectTo: '/home', pathMatch: 'full' },
--- COMP229-MoonsterDigital-Frontend/tournament/src/app/app.module.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/app.module.ts
@@ -2,25 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 
-import { HomeModule } from './pages/home/home.module';
-
-import { AddTournamentComponent } from './pages/add-tournament/add-tournament.component';
 import { FooterComponent } from './partials/footer/footer.component';
 import { HeaderComponent } from './partials/header/header.component';
-import { TournamentListComponent } from './tournament-list/tournament-list.component';
 
-
-import { CommentComponent } from './pages/comment/comment.component';
-import { ForumCommentsComponent } from './pages/forum-comments/forum-comments.component';
-import { TopicComponent } from './pages/topic/topic.component';
-
-import { RouterModule, Routes } from '@angular/router';
-import { AddEditCommentComponent } from './pages/add-edit-comment/add-edit-comment.component';
-import { ForumTopicsComponent } from './pages/forum-topics/forum-topics.component';
-import { AddEditTopicComponent } from './pages/add-edit-topic/add-edit-topic.component';
-
-import { JwtModule, JwtHelperService, JwtInterceptor } from '@auth0/angular-jwt';
+import { HomeModule } from './pages/home/home.module';
 import { AdminModule } from './admin/admin.module';
 import { ForumModule } from './forum/forum.module';
 import { TournamentModule } from './tournament/tournament.module';
@@ -28,36 +15,19 @@ import { TournamentModule } from './tournament/tournament.module';
 import { TopicRepo } from 'src/app/model/topic.repository';
 import { CommentRepo } from './model/comment.repository';
 
-export function jwtTokenGetter(): string
+import { AuthService } from './model/auth.service';
+import { TournamentRepo } from './model/tournament.repository';
+
+/*export function jwtTokenGetter(): string
 {
   return "12345";//localStorage.getItem('id_token');
-}
-
-//Routing
-const routes: Routes = [
-  { path: 'add-tournament', component: AddTournamentComponent },
-  //{ path: '', component: HomeComponent },
-  { path: 'forum-comments', component: ForumCommentsComponent },
-  { path: 'forum/add-edit-comment', component: AddEditCommentComponent },
-  { path: 'forum/forum-topics', component: ForumTopicsComponent },
-  { path: 'forum/add-edit-topic', component: AddEditTopicComponent },
-
-];
+}*/
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTournamentComponent,
     FooterComponent,
     HeaderComponent,
-    TournamentListComponent,
-    ForumCommentsComponent,
-    CommentComponent,
-    TopicComponent,
-    AddEditCommentComponent,
-    ForumTopicsComponent,
-    AddEditTopicComponent,
-    
   ],
   imports: [
     BrowserModule,
@@ -66,15 +36,14 @@ const routes: Routes = [
     AdminModule,
     ForumModule,
     TournamentModule,
-    RouterModule.forRoot(routes),
 
-    JwtModule.forRoot({
+    /*JwtModule.forRoot({
       config: {
         tokenGetter: jwtTokenGetter
       }
-    })
+    })*/
   ],
-  providers: [TopicRepo, CommentRepo],
+  providers: [TopicRepo, CommentRepo, TournamentRepo, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
