5c7bc44145b592f3d49fa4fb456a79eb6b8a6793
Yuk Yee Wong <ywong113@my.centennialcollege.ca>
2022-04-01 17:55:31 -0400

    Recreate a login component to replace the auth component

--- COMP229-MoonsterDigital-Frontend/tournament/src/app/admin/login/login.component.spec.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/admin/login/login.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TopicComponent } from './topic.component';
+import { LoginComponent } from './login.component';
 
-describe('TopicComponent', () => {
-  let component: TopicComponent;
-  let fixture: ComponentFixture<TopicComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TopicComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TopicComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
