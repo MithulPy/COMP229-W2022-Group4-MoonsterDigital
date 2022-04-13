4f9e072aa068627c6dbdc4cf7172deb2c98b36f5
MithulPy <mithulofficial@gmail.com>
2022-04-13 14:13:45 +0300

    Return to Tourney List

--- COMP229-MoonsterDigital-Frontend/tournament/src/app/tournament/bracket-final-edit/bracket-final-edit.component.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/tournament/bracket-final-edit/bracket-final-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-bracket-final-edit',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BracketFinalEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(    private router: Router  ) { }
 
   ngOnInit(): void {
   }
-
+  returnToTournamentList() {
+    this.router.navigateByUrl('/tournament/list');
+  }
 }
--- COMP229-MoonsterDigital-Frontend/tournament/src/app/tournament/bracket-qf-edit/bracket-qf-edit.component.ts
+++ COMP229-MoonsterDigital-Frontend/tournament/src/app/tournament/bracket-qf-edit/bracket-qf-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-bracket-qf-edit',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BracketQfEditComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router ) { }
 
   ngOnInit(): void {
   }
 
+  returnToTournamentList() {
+    this.router.navigateByUrl('/tournament/list');
+  }
 }
