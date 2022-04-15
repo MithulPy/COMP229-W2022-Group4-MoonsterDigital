d27a2ebbe1598780dc0b8524f1f96434c604f1a1
DiegoHdz7 <diego.hdz.glez@gmail.com>
2022-04-15 14:34:35 -0400

    added passport auth middleware to post routs of rounds

--- COMP229-MoonsterDigital-Backend/server/routes/rounds.js
+++ COMP229-MoonsterDigital-Backend/server/routes/rounds.js
@@ -9,14 +9,18 @@
 
  var express = require('express');
  var router = express.Router();
+
+ let jwt = require('jsonwebtoken');
+
+ let passport = require('passport');
  
  let roundsController = require('../controllers/rounds');
  
  /** GET route to home */
  router.get('/:tournamentId', roundsController.displayRounds);
- router.post('/set-semiFinal', roundsController.processUpsertSemiFinal);
- router.post('/set-final', roundsController.processUpsertFinal);
- router.post('/set-winner', roundsController.processUpsertWinner);
+ router.post('/set-semiFinal',passport.authenticate('jwt', {session: false}), roundsController.processUpsertSemiFinal);
+ router.post('/set-final',passport.authenticate('jwt', {session: false}), roundsController.processUpsertFinal);
+ router.post('/set-winner',passport.authenticate('jwt', {session: false}), roundsController.processUpsertWinner);
 
  //Just dev routes
  router.get('/getRounds', roundsController.getRounds);
