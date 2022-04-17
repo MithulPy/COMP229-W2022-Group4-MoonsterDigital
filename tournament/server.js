5f4124d35d43ec4957f8c81ad966ac6c274ed241
MithulPy <mithulofficial@gmail.com>
2022-04-17 05:38:22 +0300

    Git docs

--- tournament/server.js
+++ tournament/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tournament'));
+app.use(express.static('./docs'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tournament/'}),
+    res.sendFile('index.html', {root: 'docs/'}),
 );
 
 // Start the app by listening on the default Heroku port
